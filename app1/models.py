180f2ed7fe1188e50f9590050f1c636769aaa1d5
s1f102200941 <s1f102200941@iniad.org>
2022-12-16 17:52:17 +0900

    クラスUser・Article・TimeforOfficialの初期定義

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+class User(models.Model):
+    UserID=models.CharField(max_length=200)
+    password=models.CharField(max_length=200)
+class Article(models.Model):
+    ArticleID=models.CharField(max_length=200)
+    userID=models.CharField(max_length=200)
+    ArticleData=models.TextField()
+class TimeforOfficial:
+    UserID=models.CharField(max_length=200)
+    time=models.DateTimeField(default=timezone.now)
 
 # Create your models here.
