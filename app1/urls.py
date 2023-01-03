7b9060340317c84efe080059695c81533be5162b
s1f102200941 <s1f102200941@iniad.org>
2023-01-03 17:44:50 +0900

    訂正
    import文を削除しました。

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from app1 import views
-
+from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("createAccount", views.create_account, name="createAccount"),
