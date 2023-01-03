089628f7a37243f9901b2b3439aff06b1caeaf02
s1f102200941 <s1f102200941@iniad.org>
2023-01-03 17:50:47 +0900

    不要な箇所の削除
    sort関数を削除・detail文の削除

--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from app1.models import *
 import re
 from django.http import Http404, JsonResponse
+from app1.models import Article
+from .models import Article,Like
 
 
 def index(request):
@@ -36,16 +38,7 @@ def index(request):
     
         #ない場合は"ログイン"のまま返す
         return render(request, "index.html", context)
-    if ('sort' in request.GET):
-        #いいね順と新着順と難しい順と解きやすい順の並び替え
-        if request.GET['sort']=='interesting':
-            articles=Article.objects.order_by('-interesting')
-        elif request.GET['sort']=='difficult':
-            articles=Article.objects.order_by('-difficult')
-        elif request.GET['sort']=='easy':
-            articles=Article.objects.order_by('-easy')
-    else:
-        articles=Article.objects.order_by('-posted_at')
+   
 
 def create_account(request):
 
@@ -225,6 +218,7 @@ def get_answer(request):
     else:
         result = "不正解です"
     return HttpResponse(result)
+
 #面白いの関数
 def api_interesting(request,article_id):
     try:
